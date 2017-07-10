f52aaae2d0da2bace876c2616d62cdf8e47a05a2
dengliang09 <642782076@qq.com>
2017-07-10 09:56:53 +0800

    --

---
+++ Wokog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wokog
+//
+//  Created by dengliang on 17/7/10.
+//  Copyright © 2017年 邓亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wokog/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Wokog
+//
+//  Created by dengliang on 17/7/10.
+//  Copyright © 2017年 邓亮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
